2010b3758c7155c7140f4a0c70a1db1a88698204
Pookieh <134815513+Pookieh@users.noreply.github.com>
2025-07-15 00:59:44 -0500

    estructura tile
    todavía falta poner bien todo y las animaciones pero puedes ir poniendo los scripts

---
+++ Assets/notas.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class notas : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // usa dos muros, uno que cubra la altura normal y el segundo hacia arriba como si fuera otro tile, solo para que se sobre ponga al player
+        // es una nota para mi
+        
+
+    }
+}
+
